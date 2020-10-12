8415339c3dcf96d0949a5dbf605c3d0eed4d9545
Pythonidaer <pythonidaer@gmail.com>
2020-10-12 19:31:32 -0400

    Created a toggle menu but didn't figure out the fadeIn fadeOut JavaScript syntax.

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,20 @@
 function myFunction() {
     var x = document.getElementsByClassName("hamburger");
-    if (x[0].className === "hamburger hamburger--spin mobile") {
+    var y = document.getElementsByClassName("nav-hidden");
+    var a = y.classList;
+    console.log(y);
+    
+
+    if (x[0].className === "hamburger hamburger--spin mobile" && y[0].className === "nav-fade nav-hidden") {
       x[0].className += " is-active";
+      // y[0].className += " nav-visible";
+      // y[0].classList.add('nav-fade');
+      y[0].classList.add('nav-visible');
+
+
+
     } else {
       x[0].className = "hamburger hamburger--spin mobile";
+      y[0].className = "nav-fade nav-hidden";
     }
   }
